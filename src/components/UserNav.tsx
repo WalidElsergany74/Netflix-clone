83b61a63f26176f63186d90ba0ceb26a8e1e8cfa
WalidElsergany74 <125218561+WalidElsergany74@users.noreply.github.com>
2025-02-02 18:40:02 +0200

    Update UserNav.tsx

--- src/components/UserNav.tsx
+++ src/components/UserNav.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {  signOut, useSession } from "next-auth/react";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
