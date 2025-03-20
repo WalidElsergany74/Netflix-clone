6cdb0e6c1c3b207e2a960c6085e660dc9465cec8
Walid Elsergany <125218561+WalidElsergany74@users.noreply.github.com>
2025-03-20 17:44:49 +0200

    Update NextAuthProvider.tsx

--- src/Providers/NextAuthProvider.tsx
+++ src/Providers/NextAuthProvider.tsx
@@ -3,7 +3,7 @@
 import { SessionProvider } from "next-auth/react";
 
 function NextAuthSessionProvider({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <SessionProvider>{children}</SessionProvider> ;
 }
 
 export default NextAuthSessionProvider;
